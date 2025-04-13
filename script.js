56df8b6c1d3de6c078965cdc457696404bf3b310
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-04-13 22:07:36 +0100

    more twwerk

---
+++ script.js
@@ -0,0 +1,25 @@
+
+ const birthday = new Date("2025-04-14T00:00:00").getTime();
+
+ const updateCountdown = () => {
+   const now = new Date().getTime();
+   const gap = birthday - now;
+
+   const second = 1000;
+   const minute = second * 60;
+   const hour = minute * 60;
+   const day = hour * 24;
+
+   const d = Math.floor(gap / day);
+   const h = Math.floor((gap % day) / hour);
+   const m = Math.floor((gap % hour) / minute);
+   const s = Math.floor((gap % minute) / second);
+
+   document.getElementById("days").innerText = d;
+   document.getElementById("hours").innerText = h;
+   document.getElementById("minutes").innerText = m;
+   document.getElementById("seconds").innerText = s;
+ };
+
+ updateCountdown();
+ setInterval(updateCountdown, 1000);
