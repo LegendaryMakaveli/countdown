51e66132663251a59f9ae2976571d11f7a33469b
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-04-14 11:25:01 +0100

    how update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
- const birthday = new Date("2025-04-14T00:00:00").getTime();
+ const birthday = new Date("2025-04-30T00:00:00").getTime();
 
  const updateCountdown = () => {
    const now = new Date().getTime();
